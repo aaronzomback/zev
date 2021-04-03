534d0459fb88b00cef4a1ac08d2273682870b3a3
aaronzomback <aaronzomback@gmail.com>
2021-04-03 11:20:00 +0200

    nav/header

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def about
+  end
+
+  def testimonials
+  end
+
+  def subjects
+  end
+
+  def pricing
+  end
+
+  def contact
+  end
+  
+end
