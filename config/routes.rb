a61a80fa2ad089ccddea36ea5efc8c6ab4038268
aaronzomback <aaronzomback@gmail.com>
2021-04-03 12:32:13 +0200

    contact styling button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-
+  get "contact", to: "pages#contact"
+  get "subjects", to: "pages#subjects"
  root "pages#home"
 
 end
